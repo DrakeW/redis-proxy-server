1de60d68d2f73f1f5360afd30baefe9f41adf662
Junyu Wang <junyuw@berkeley.edu>
2019-07-30 00:05:18 -0700

    added logging and get key from redis

--- proxy/server.go
+++ proxy/server.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 
 	"github.com/go-redis/redis"
+	"github.com/sirupsen/logrus"
 )
 
 const (
@@ -30,40 +31,73 @@ type Config struct {
 type server struct {
 	redisdb *redis.Client
 	config  Config
+	logger  *logrus.Logger
 }
 
 // newServer returns a new server object based on the proxy config input
 func newServer(config Config) *server {
+	// initialize logging
+	logger := logrus.New()
+	logger.Formatter = &logrus.TextFormatter{
+		FullTimestamp: true,
+	}
+	// set up redis client
 	if config.RedisAddr == "" {
-		log.Fatal("Address of redis instance cannot be empty")
+		logger.Fatal("address of redis instance cannot be empty")
 	}
 	client := redis.NewClient(&redis.Options{
 		Addr:     config.RedisAddr,
 		Password: "", // no password set
 		DB:       0,  // use default DB
 		OnConnect: func(con *redis.Conn) error {
-			log.Print("Connection to redis established at ", config.RedisAddr)
+			logger.Info("connection to redis established at ", config.RedisAddr)
 			return nil
 		},
 	})
 	_, err := client.Ping().Result()
 	if err != nil {
-		log.Fatal("Failed to connect to redis at ", config.RedisAddr)
+		logger.Fatal("failed to connect to redis at ", config.RedisAddr)
 	}
+
 	return &server{
 		redisdb: client,
 		config:  config,
+		logger:  logger,
 	}
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "hello world at %s", r.URL.Path[1:])
+// API - Method: GET - Endpoint - /get?key=<key>
+func (s *server) getKey(w http.ResponseWriter, r *http.Request) {
+	key := r.URL.Query().Get("key")
+
+	// read from redis
+	val, err := s.getKeyFromRedis(key)
+	if err == redis.Nil {
+		http.Error(w, fmt.Sprintf("Entry with key %s doesn't exist", key), http.StatusNotFound)
+	} else if err != nil {
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+	} else {
+		fmt.Fprint(w, val)
+	}
+}
+
+// getKeyFromRedis performs a GET command with a key to redis
+func (s *server) getKeyFromRedis(key string) (string, error) {
+	val, err := s.redisdb.Get(key).Result()
+	if err == redis.Nil {
+		return "", err
+	} else if err != nil {
+		s.logger.Error("failed to retrieve key ", key, "from redis - error: ", err.Error())
+		return "", err
+	} else {
+		return val, nil
+	}
 }
 
 // Run starts the proxy web server
 func Run(config Config) {
 	server := newServer(config)
-	http.HandleFunc("/", handler)
+	http.HandleFunc("/get", server.getKey)
 
 	addr := fmt.Sprintf(":%d", server.config.ListenPort)
 	log.Fatal(http.ListenAndServe(addr, nil))
